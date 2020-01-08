9fee7e4ada73e78933753b749577bccb790853b1
yquyet <quyet01012305>
2020-01-08 11:01:32 +0700

    update

---
+++ src/main/java/pages/OpenAccountPage.java
@@ -0,0 +1,38 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
+
+public class OpenAccountPage {
+
+    @FindBy(xpath = "//*[@id=\"userSelect\"]")
+    WebElement clickChose;
+
+    @FindBy(xpath = "/html/body/div[3]/div/div[2]/div/form/div/select/option[3]")
+    WebElement clickChoseName;
+
+    @FindBy(xpath = "/html/body/div[3]/div/div[2]/div/form/button")
+    WebElement btnLogin;
+
+
+    public void chose(){
+        this.clickChose.click();
+
+    }
+
+
+    public void choseName(){
+        this.clickChoseName.click();
+
+    }
+
+    public void submitLogin(){
+        btnLogin.click();
+    }
+
+
+
+
+
+}
